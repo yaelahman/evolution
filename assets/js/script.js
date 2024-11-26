aaab6aacd31b23f243dd1ccd3816412f8239d21c
Manzcode <yaelahman0810@gmail.com>
2024-11-26 07:13:49 +0700

    init project

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                header: '#012355',
+                muted: '#677B99',
+                primary: '#0077B6',
+                secondary: '#1D73F6',
+                gray: '#F9F9F9',
+            },
+            backgroundImage: {
+                primaryGradient: 'linear-gradient(to right, #0065AE -20%, #1D73F5 17%, #1D73F5 51%, #1D73F5 76%, #0077B6 113%)',
+                progress: 'linear-gradient(to right, #0CA3FF 0%, #024DBB 100%)'
+            }
+        },
+    },
+};
